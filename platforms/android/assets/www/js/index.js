9638e28acdc39ff26aa3d001e63ae9fa236619e3
weekheongquape <wee.kheong@quape.com>
2015-12-12 21:06:24 +0800

    try

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -33,9 +33,16 @@ var app = {
     },
     receivedEvent: function(id) {
         $('button#signup').click(function(e){
+			try{
 			$.post("http://42.61.224.110:8080/carismatic/index.php/action/adduser",{username:$('#signup_username').val(),password:$('#signup_password').val(),email:$('#signup_email').val(),rights:$('#signup_accounttype').val()},function(request){
 				alert(request);
 			});
+			}
+			catch(e)
+			{
+				alert(e.message);
+			}
+			
 		});
     }
 };
