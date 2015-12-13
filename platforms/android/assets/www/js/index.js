baaa2f0c82902e030a06e17cc283a175677d9834
weekheongquape <wee.kheong@quape.com>
2015-12-13 23:52:31 +0800

    test

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -70,7 +70,7 @@ var app = {
 			// Options: throw an error if no update is received every 30 seconds.
 			//
 			try{
-				var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+				var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { maximumAge: 0, timeout: 30000, enableHighAccuracy:true });
 			}
 			catch(e)
 			{
